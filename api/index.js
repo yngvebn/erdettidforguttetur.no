918c3a2aac2ba482bbd6535f8435d15c93661674
Yngve Bakken-Nilsen <yngve.nilsen@gmail.com>
2021-03-10 14:19:48 +0100

    api

---
+++ api/index.js
@@ -0,0 +1,17 @@
+const texts = [
+    'Nope',
+    'Nei, ikke akkurat nå...',
+    'Kanskje vente til pandemien er over?',
+    'NEI!',
+    'Nah',
+    'Helst ikke',
+    `<a href="https://bit.ly/2PKucCJ" target="_blank">Les mer her!</a>`
+]
+
+exports.handler = async function (event, context) {
+    const text = Math.floor(Math.random() * 7);
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ message: text })
+    };
+}
